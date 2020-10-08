c285cab20d42bff145638c2ca5f53c3db0f10a23
heidy <helly228@hotmail.com>
2020-10-08 17:23:06 -0500

    fci background css

--- src/components/careerPath/CareerPath.js
+++ src/components/careerPath/CareerPath.js
@@ -1,36 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import avatar from "../../images/Avatar.png";
-import Next from "../shared/Next"
+import Next from "../shared/Next";
 import Range from "./Range";
-import "../../style/careerPath.css"
-
+import "../../style/careerPath.css";
 
 function CareerPath(state) {
-  let name= state.location.state.user
-  console.log(name)
+  let name = state.location.state.user;
+  console.log(name);
   return (
-    <div className="container-fluid bg-dark career vh-100 ">
-      <div className="row p-4 vh-100">
+    <div className="container-fluid vh-100 bg-dark ">
+      <div className="row p-4">
         <section className="col-12 col-md-6">
           <div className="d-flex align-items-center justify-content-around">
-          <h1 className="text">Career Path</h1>
-         
-          <Next
-          styleArrow="arrow text"
-          to={{
-            pathname: "./Triforce",
-            state: {user:name}
-          }}>
-          </Next>
+            <h1 className="text">Career Path</h1>
+            <Next
+              styleArrow="arrow text"
+              to={{
+                pathname: "./Triforce",
+                state: { user: name },
+              }}
+            ></Next>
           </div>
           <hr className="hr"></hr>
-          <div className="col-12  d-flex justify-content-center ">
-            <img src={avatar} className="w-50 h-100"></img>
+          <div className="col-12  d-flex justify-content-center">
+            <img src={avatar} className="w-50"></img>
           </div>
         </section>
-        <section className="col-12 col-md-6">
-          <div className="d-flex h-100 align-items-center">
-          <Range />
+        <section className="col-12 col-md-6 ">
+          <div className="d-flex h-100 align-items-center ">
+            <Range />
           </div>
         </section>
       </div>
